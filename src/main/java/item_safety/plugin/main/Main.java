3614769499d50ae64bf4ad353b052c07d2111ee4
maguro027 <maguro027@gmail.com>
2022-07-02 04:00:15 +0900

    master

--- src/main/java/item_safety/plugin/main/Main.java
+++ src/main/java/item_safety/plugin/main/Main.java
@@ -1,77 +1,84 @@
 package item_safety.plugin.main;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-
-
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.json.simple.JSONObject;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class Main {
 
-	static HashMap<Integer, ItemStack> items = new HashMap<>();
+	public static List<Material> items = new ArrayList<Material>();
 	public static final File file_ = new File(new File("").getAbsolutePath().toString() + "/plugins/item_safety/");
 
 	public static void loadconfig() {
 		if (!(file_.exists())) file_.mkdir();
-		try(BufferedReader br = new BufferedReader(new FileReader(file_ + "/main_menu.json"))) {
+		items.clear();
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(file_ + "/main_menu.json"));
+			items = new Gson().fromJson(reader, new TypeToken<List<Material>>() {
+			}.getType());
 
-            String line;
-            while ((line = br.readLine()) != null) {
-            }
+			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
-	public static void saveconfig(JSONObject json) {
+	public static void saveconfig(String string) {
 		if (!(file_.exists())) file_.mkdir();
 		try (FileWriter writer = new FileWriter(file_ + "/main_menu.json")) {
-			writer.write(json.toString());
+			writer.write(string);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static void setinventory(Player player) {
-		Inventory inv = Bukkit.createInventory(null, 54, "Item_Safety");
-		int conut = -1;
+		Inventory inventory = Bukkit.createInventory(null, 54, "Item_Safety");
+		if (items == null) player.openInventory(inventory);
+
 		try {
-			for (java.util.Map.Entry<Integer, ItemStack> item1 : items.entrySet()) {
-				inv.setItem(item1.getKey(), item1.getValue());
+			int inv_i = 0;
+			for (Material item : items) {
+				inventory.setItem(inv_i, new ItemStack(item, 1));
+				inv_i++;
 			}
 
-			for (int i = 0, size = inv.getSize(); i < size; ++i) {
-				if (inv.getItem(i) == null) continue;
-				conut++;
-				items.put(conut, inv.getItem(i));
+			for (int i = 0, size = inventory.getSize(); i < size; ++i) {
+				if (inventory.getItem(i) == null) continue;
+				items.add(inventory.getItem(i).getType());
 			}
-			player.openInventory(inv);
+			player.openInventory(inventory);
 		} catch (ArrayIndexOutOfBoundsException items) {// hash map null
-			player.openInventory(inv);
+			player.openInventory(inventory);
 		}
 	}
 
 	public static void closeInventory(Player player, Inventory inventory) {
-		// d•¡‚ðíœ‚µ‚½‚¢
+		Gson gson = new Gson();
 		int conut = -1;
 		items.clear();
 		for (int i = 0, size = inventory.getSize(); i < size; ++i) {
 			if (inventory.getItem(i) == null) continue;
 			conut++;
-			items.put(conut, inventory.getItem(i));
+			items.add(inventory.getItem(i).getType());
 		}
-		System.out.println(new JSONObject(items).toString());
-		saveconfig(new JSONObject(items));
+		items = new ArrayList<Material>(new LinkedHashSet<>(items));
+		if (conut == -1) return;
+		saveconfig(gson.toJson(items));
 	}
 
 	@SuppressWarnings("deprecation")
